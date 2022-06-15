11e1ab9efc72113e5f2a90f7e4fb52fe8eb4c298
Sai Seng <saisengmongli14@gmail.com>
2022-06-15 13:59:06 +0630

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -42,6 +42,42 @@ const Navbar = () => {
 				</li>
 			</div>
 			<div className="cart"></div>
+			<div className="burger-menu">
+				<li>
+					<Link href="/"><a>HOME</a></Link>
+					<ul>
+						<li><Link href="/home/phase"><a>PHASE-01</a></Link></li>
+						<li><Link href="/home/vss1"><a>LOI TAI X VLONE - VSS1</a></Link></li>
+						<li><Link href="/home/mwm"><a>MEET WITH MAYA</a></Link></li>
+					</ul>
+				</li>
+
+				<li>
+					<Link href=""><a>SHOP</a></Link>
+					<ul>
+						<li><Link href="/shop/"><a>ALL PRODUCT</a></Link></li>
+						<li><Link href="/shop/"><a>IN STOCK</a></Link></li>
+						<li><Link href="/shop/"><a>LATEST</a></Link></li>
+					</ul>
+				</li>
+
+				<li>
+					<Link href=""><a>FOCUS</a></Link>
+					<ul>
+						<li><Link href="/focus/archive"><a>ARCHIVE</a></Link></li>
+						<li><Link href="/focus/lookbook"><a>LOOKBOOK</a></Link></li>
+					</ul>
+				</li>
+
+				<li>
+					<Link href=""><a>EXTRA</a></Link>
+					<ul>
+						<li><Link href="/extra/subscribe"><a>SUBSCRIBE</a></Link></li>
+						<li><Link href="/extra/stockist"><a>STOCKIST</a></Link></li>
+						<li><Link href="/extra/terms"><a>TERMS</a></Link></li>
+					</ul>
+				</li>
+			</div>
 			
 		</nav>
 	)
